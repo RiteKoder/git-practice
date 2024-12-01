60f3a2a556cd252c13f67de5bf1b7e629212c0c0
RiteKoder <Riteshkrpandawork@gmail.com>
2024-12-01 22:17:30 +0530

    add test component

--- React_Projects/first-app/my-react-app/src/App.jsx
+++ React_Projects/first-app/my-react-app/src/App.jsx
@@ -1,10 +1,12 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import Test from "./New";
 
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <Test />
+      <h1>Hello World</h1>
+    </>
+  );
 }
 
 export default App;
---
+++ React_Projects/first-app/my-react-app/src/New.jsx
@@ -0,0 +1,5 @@
+function Test() {
+  return <h3>Test component</h3>;
+}
+
+export default Test;
